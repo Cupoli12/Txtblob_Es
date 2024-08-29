99742cebb5a1832fb8e8455cec728cd720a1b3ee
Cupoli12 <167145381+Cupoli12@users.noreply.github.com>
2024-08-29 10:30:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,38 @@
 import streamlit as st
 from textblob import TextBlob
-from googletrans import Translator
+from textblob_es import PatternAnalyzer
 
-translator = Translator()
-st.title('Uso de textblob')
+st.title('Análisis de Sentimiento en Español')
 
 st.subheader("Por favor escribe en el campo de texto la frase que deseas analizar")
+
 with st.sidebar:
-               st.subheader("Polaridad y Subjetividad")
-               ("""
-                Polaridad: Indica si el sentimiento expresado en el texto es positivo, negativo o neutral. 
-                Su valor oscila entre -1 (muy negativo) y 1 (muy positivo), con 0 representando un sentimiento neutral.
-                
-               Subjetividad: Mide cuánto del contenido es subjetivo (opiniones, emociones, creencias) frente a objetivo
-               (hechos). Va de 0 a 1, donde 0 es completamente objetivo y 1 es completamente subjetivo.
+    st.subheader("Polaridad y Subjetividad")
+    st.write("""
+        **Polaridad**: Indica si el sentimiento expresado en el texto es positivo, negativo o neutral. 
+        Su valor oscila entre -1 (muy negativo) y 1 (muy positivo), con 0 representando un sentimiento neutral.
 
-                 """
-               ) 
-
+        **Subjetividad**: Mide cuánto del contenido es subjetivo (opiniones, emociones, creencias) frente a objetivo
+        (hechos). Va de 0 a 1, donde 0 es completamente objetivo y 1 es completamente subjetivo.
+    """)
 
 with st.expander('Analizar Polaridad y Subjetividad en un texto'):
     text1 = st.text_area('Escribe por favor: ')
     if text1:
-
-        #translation = translator.translate(text1, src="es", dest="en")
-        #trans_text = translation.text
-        #blob = TextBlob(trans_text)
-        blob = TextBlob(text1)
-       
-        
-        st.write('Polarity: ', round(blob.sentiment.polarity,2))
-        st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
-        x=round(blob.sentiment.polarity,2)
+        # Usar PatternAnalyzer para español
+        blob = TextBlob(text1, analyzer=PatternAnalyzer())
+        st.write('Polaridad: ', round(blob.sentiment[0], 2))
+        st.write('Subjetividad: ', round(blob.sentiment[1], 2))
+        x = round(blob.sentiment[0], 2)
         if x >= 0.5:
-            st.write( 'Es un sentimiento Positivo 😊')
+            st.write('Es un sentimiento Positivo 😊')
         elif x <= -0.5:
-            st.write( 'Es un sentimiento Negativo 😔')
+            st.write('Es un sentimiento Negativo 😔')
         else:
-            st.write( 'Es un sentimiento Neutral 😐')
+            st.write('Es un sentimiento Neutral 😐')
 
-with st.expander('Corrección en inglés'):
-       text2 = st.text_area('Escribe por favor: ',key='4')
-       if text2:
-          blob2=TextBlob(text2)
-          st.write((blob2.correct())) 
+with st.expander('Corrección en español'):
+    text2 = st.text_area('Escribe por favor: ', key='4')
+    if text2:
+        blob2 = TextBlob(text2)
+        st.write(blob2.correct())
